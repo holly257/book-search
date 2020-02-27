e17c829bd28db7e6b3f58824dd56e5f75528398a
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-27 13:15:45 -0500

    handling for err and price formatting

--- src/Results/eachResult.js
+++ src/Results/eachResult.js
@@ -2,27 +2,46 @@ import React from 'react';
 import './eachResult.css';
 import ExpandResult from './ExpandResult';
 
-function EachResult(props){
+class EachResult extends React.Component{
+    filterPrice(price){
+        console.log(price)
+        if(!price){
+            return 'Price is not available'
+        } else if(price.listPrice) {
+            return 'Price: $ ' + price.listPrice.amount
+        } else if (price.saleability.toUpperCase() === 'FREE') {
+            return 'Price: ' + price.saleability
+        } else {
+            return 'Pricing not known'
+        }
+    }
     
-    console.log(props)
-    const maxLength = 180;
-    return(
-        <div id='each-result-box'>
-            <h3  className='child' id='title'>{props.title}</h3>
-            <img  id='book-image' src={props.image} alt='book image' />
-            <p className='details child' id='author'>Author: {props.author ? props.author : 'author not available'}</p>
-            <p className='details child'>Price: ${props.price ? 
-                props.price.amount : props.price.saleability} </p>
-            <div className='details child' id='description'>
-                {!props.description ? 'description not available' 
-                    : props.description.length > maxLength ? 
-                        [props.description.substring(0, maxLength) + ' ...']
-                        : props.description
-                }
-            </div> 
-            <button id='read-more-btn' className='details child'>Read More</button>
-        </div>  
-    )
+    render (){
+        const maxLength = 180;
+        return(
+            <div id='each-result-box'>
+                <h3  className='child' id='title'>{this.props.title}</h3>
+                <img  id='book-image' src={this.props.image} alt='book image' />
+                <p className='details child' id='author'>Author: {this.props.author ? this.props.author : 'author not available'}</p>
+                <p className='details child'>
+                    {this.filterPrice(this.props.price)} </p>
+                    
+                <div className='details child' id='description'>
+                    {!this.props.description ? 'description not available' 
+                        : this.props.description.length > maxLength ? 
+                            [this.props.description.substring(0, maxLength) + ' ...']
+                            : this.props.description
+                    }
+                </div> 
+                <button id='read-more-btn' className='details child'>Read More</button>
+            </div>  
+        )
+    }
 }
 
 export default EachResult;
+
+
+// <p className='details child'>
+// Price: ${props.price ? props.price.listPrice.amount 
+//     : !props.price ? props.price.saleability : 'price not available'} </p>
